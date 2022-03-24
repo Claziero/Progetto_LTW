ef1591908b446bcd7482e094e1e50080afa66a8d
Claziero <lorenzoclazzer.2000@outlook.it>
2022-03-24 18:22:20 +0100

    Modificati header e pagina di login

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+    <title>Index</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/style-main.css"/>
+
+    <script type="application/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+    <script type="application/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    
+    <!-- Per redirezionare ad una pagina 
+    <meta http-equiv="refresh" content="0; pages/login.html"/>-->
+</head>
+
+<body class="text-center">
+
+    <!-- Includi l'header -->
+    <?php include_once "pages/utils/header.php"; ?>
+    
+
+
+        <!-- Includi l'header -->
+    <?php include_once "pages/utils/footer.html"; ?>
+
+</body>
+
+</html>
--- pages/utils/header.php
+++ pages/utils/header.php
@@ -2,17 +2,14 @@
 <header class="p-3 mb-3 border-bottom">
     <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <a href="#" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
-                <img src="bootstrap/assets/img/favicons/favicon-32x32.png" alt="mdo" width="40" height="40"
-                    class="rounded-3">
+            <a href="#" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none me-lg-3">
+                <img src="bootstrap/assets/img/favicons/favicon-32x32.png" alt="mdo" width="40" height="40" class="rounded-3">
             </a>
 
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                 <li><a href="#" class="nav-link px-2 link-dark">Highlights</a></li>
                 <li><a href="#" class="nav-link px-2 link-dark">Prossimamente</a></li>
                 <li><a href="#" class="nav-link px-2 link-dark">Eventi conclusi</a></li>
-                <li><a href="#" class="nav-link px-2 link-dark">FAQ</a></li>
-                <li><a href="#" class="nav-link px-2 link-dark">Contatti</a></li>
             </ul>
 
             <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
@@ -26,7 +23,7 @@
                 </a>
                 <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
                     <li><a class="dropdown-item" href="#">Impostazioni</a></li>
-                    <li><a class="dropdown-item" href="#">Il mio profilo</a></li>
+                    <li><a class="dropdown-item" href="pages/login.html">Il mio profilo</a></li>
                     <li>
                         <hr class="dropdown-divider">
                     </li>
