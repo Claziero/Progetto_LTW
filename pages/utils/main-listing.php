7f25c69f282e69e22fe01a11269b1c365473791b
Claziero <lorenzoclazzer.2000@outlook.it>
2022-03-27 13:17:34 +0200

    Aggiornato stile dei pulsanti. Footer fissato in basso

--- pages/utils/main-listing.php
+++ pages/utils/main-listing.php
@@ -1,10 +1,14 @@
-<div class="jumbotron container border border-dark rounded mb-3">
-    <h1 class="display-5">Nuovo evento</h1>
+<div class="listing jumbotron container border border-dark rounded mb-3">
+    <h3 class="display-6">Nuovo evento</h3>
     <p class="lead">Evento xy il 20/05/22 19:00</p>
-    <hr class="my-2">
+    <hr class="my-1">
     <p>Descrizione evento.</p>
     <p class="lead">
-        <a class="btn btn-primary btn-lg" href="#" role="button">Maggiori informazioni</a>
-        <button class="btn btn-primary btn-lg">Prenota</button>
+        <a href="#" class="px-1">
+            <button class="btn btn-info btn-lg">Maggiori informazioni</button>
+        </a>
+        <a href="#" class="">
+            <button class="btn btn-prenota btn-lg">Prenota</button>
+        </a>
     </p>
 </div>
