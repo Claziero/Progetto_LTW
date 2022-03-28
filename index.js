4d486b41119f715eb3f44501323cd1a3c0ad5a8f
Claziero <lorenzoclazzer.2000@outlook.it>
2022-03-28 15:18:04 +0200

    Aggiunta la visualizzazione del login Inseriti placeholders in main.hbr

--- index.js
+++ index.js
@@ -7,10 +7,27 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+
+// Per usare file di stile css e script javascript
 app.use(express.static("public"));
 
+// Render della homepage
 app.get('/', (req, res) => {
-    res.render('home', {tp:["tr", 0]});
+    res.render('home', {
+        title: "Home", 
+        style: "style-main.css",
+        js: "homeActions.js"
+    });
 });
 
+// Render della pagina di login
+app.get('/login', (req, res) => {
+    res.render('login', {
+        title: "Login", 
+        style: "style-signin.css",
+        js: "validateSignin.js"
+    });
+});
+
+// Porta 3000 per il server
 app.listen(3000);
