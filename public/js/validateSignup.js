3fb20a8d0f8b0aabf990261e2fd5b60dcb4a8d1f
andrea1199 <andreavallonejr@gmail.com>
2022-03-29 23:50:15 +0200

    create funzioni per controllo di psw e campo sesso, da verificare funzione.lenght in esecuzione

---
+++ public/js/validateSignup.js
@@ -0,0 +1,46 @@
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function (event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+
+
+var check = function () {
+    if (document.getElementById('password').value ==
+        document.getElementById('password_repeat').value) {
+        document.getElementById('message').style.color = 'green';
+        document.getElementById('message').innerHTML = 'matching';
+    } else {
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerHTML = 'not matching';
+    }
+}
+
+var checkLgh = function () {
+    if ((document.getElementById('password').innerHTML.length) < 6) {
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerHTML = 'troppo corta';
+    } else {
+        document.getElementById('message').style.color = 'green';
+        document.getElementById('message').innerHTML = 'lunghezza OK';
+
+    }
+
+
+}
+
+var checkSex = function () {
+    if ((document.getElementById('sesso').innerHTML.length) != 1) {
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerHTML = 'inserire singolo carattere per sesso';
+    }
+    if ((document.getElementById('sesso').value!='M') && (document.getElementById('sesso').value!='F') && (document.getElementById('sesso').value!='U') && (document.getElementById('sesso').value!='N') ){
+        document.getElementById('message').style.color = 'red'
+        document.getElementById('message').innerHTML = 'inserire M = maschio, F = femmina, N = non binario, U = non definito';
+    }
+
+
+}
+
