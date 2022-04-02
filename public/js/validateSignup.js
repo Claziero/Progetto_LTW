dfab26e9d5162f66a3d2858d1118cc33e2763830
Claziero <lorenzoclazzer.2000@outlook.it>
2022-04-02 19:10:06 +0200

    Completata parte di inserimento utente nel db Mancante funzione di validazione data di nascita Mancante parte di errore nel caso di utente già iscritto

--- public/js/validateSignup.js
+++ public/js/validateSignup.js
@@ -6,10 +6,12 @@ function checkPwdMatch () {
         document.getElementById('message').innerHTML = '';
         document.getElementById('password').style.borderColor = 'green';
         document.getElementById('password_repeat').style.borderColor = 'green';
+        return true;
     } else {
         document.getElementById('message').style.color = 'red';
         document.getElementById('message').innerHTML = 'Le password non corrispondono';
         document.getElementById('password_repeat').style.borderColor = 'red';
+        return false;
     }
 }
 
@@ -20,13 +22,28 @@ function checkLgh () {
         document.getElementById('message').style.color = 'red';
         document.getElementById('password').style.borderColor = 'red';
         document.getElementById('message').innerHTML = 'La password deve contenere almeno 8 caratteri';
+        return false;        
     } else {
         document.getElementById('message').style.color = 'green';
         document.getElementById('password').style.borderColor = 'green';
         document.getElementById('message').innerHTML = '';
+        return true;        
     }
 }
 
+// Verifica che il sesso sia tra le scelte ammissibili
+function checkSex () {
+    var a = document.getElementById('sesso').value;
+    return (a == 'M' || a == 'F' || a == 'U');
+}
+
+// verifica che la data inserita sia valida (almeno 18 anni)
+function checkData () {
+    // TBA
+    return true;
+}
+
+// Mostra la password (1)
 function showPwd () {
     var txt = document.getElementById('password');
     if (txt.type == 'password') {
@@ -36,6 +53,7 @@ function showPwd () {
     }
 }
 
+// Mostra la password (2)
 function showPwd2 () {
     var txt = document.getElementById('password_repeat');
     if (txt.type == 'password') {
@@ -48,6 +66,5 @@ function showPwd2 () {
 // Funzione per il check del form prima dell'invio al server
 function validaForm() {
     // Controllare che tutti i campi della form siano validi
-    // In seguito redirezionare verso /signupValid
-    
+    return (checkLgh() && checkPwdMatch() && checkSex() && checkData());
 }
