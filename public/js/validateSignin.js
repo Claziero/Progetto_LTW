29197e030d8b5d3e5023dfaaa6fab84e53417fb2
andrea1199 <andreavallonejr@gmail.com>
2022-04-03 22:09:54 +0200

    cambio grafico login

--- public/js/validateSignin.js
+++ public/js/validateSignin.js
@@ -3,3 +3,13 @@
 //inputEmail
 
 //inputPassword
+
+
+function showPwd () {
+    var txt = document.getElementById('password');
+    if (txt.type == 'password') {
+        txt.type = 'text';
+    } else {
+        txt.type = 'password';
+    }
+}
