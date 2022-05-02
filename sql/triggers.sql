f8ed26af3cf43feda38acf064882f3bf2c225e05
Claziero <lorenzoclazzer.2000@outlook.it>
2022-05-02 15:07:04 +0200

    Aggiunti trigger per aggiornare proprietà evento Visualizzazione tasto prenota solo con evento disponibile Visualizzazione posti solo per eventi disponibili Aggiunti nuovi eventi

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -26,3 +26,22 @@ FOR EACH ROW EXECUTE PROCEDURE diminuisci_posti();
 
 CREATE OR REPLACE TRIGGER trigger_delete_prenotazione AFTER DELETE ON prenotazione
 FOR EACH ROW EXECUTE PROCEDURE aumenta_posti();
+
+-- Funzione per aggiornare i flag "passato" e "disponibile" in base al timestamp attuale
+CREATE OR REPLACE FUNCTION set_flags() RETURNS TRIGGER AS
+$$
+	BEGIN
+		UPDATE evento
+			SET passato = true, disponibile = false
+            WHERE dataora < (SELECT NOW()::timestamp);
+		RETURN null;
+	END;
+$$ LANGUAGE PLPGSQL;
+
+-- Triggers per attivare la funzione
+-- I flag verranno aggiornati ad ogni prenotazione inserita o tolta
+CREATE OR REPLACE TRIGGER trigger_insert_prenotazione_flags AFTER INSERT ON prenotazione
+FOR EACH ROW EXECUTE PROCEDURE set_flags();
+
+CREATE OR REPLACE TRIGGER trigger_delete_prenotazione_flags AFTER DELETE ON prenotazione
+FOR EACH ROW EXECUTE PROCEDURE set_flags();
